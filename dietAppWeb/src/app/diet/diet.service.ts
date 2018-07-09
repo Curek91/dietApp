6bed9df4d52b15a84d1e7511e2c6003cd46133fe
Tomasz Cur <tomasz.cur@sollers.eu>
2018-07-09 11:03:57 +0200

    przejscie na angular 6

--- dietAppWeb/src/app/diet/diet.service.ts
+++ dietAppWeb/src/app/diet/diet.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {Product} from './models/Product';
 import 'rxjs/add/operator/map';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class DietService {
