8645da5df6e0c91e7f87f631c799ade0a9da0c39
Tomasz Cur <tomasz.cur@sollers.eu>
2019-08-23 13:06:22 +0200

    Poprawki dodawania klientów i produktów - utworzenie sekwencji base_entity_seq w encji BaseEntity - w pliku data.sql korzystanie z sekwencji przy dodawaniu wpisów

--- dietApp/src/main/java/eu/tcitsolutions/dietApp/core/common/entity/BaseEntity.java
+++ dietApp/src/main/java/eu/tcitsolutions/dietApp/core/common/entity/BaseEntity.java
@@ -3,17 +3,15 @@ package eu.tcitsolutions.dietApp.core.common.entity;
 import lombok.Getter;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @Getter
 @MappedSuperclass
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_entity_generator")
+    @SequenceGenerator(name="base_entity_generator", sequenceName = "base_entity_seq", allocationSize=1)
     @NaturalId
     protected Long id;
 }
